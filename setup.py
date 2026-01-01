f7ba35bc9ededd846ffe7f4463d9d76de7b9c849
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2026-01-01 22:03:33 +0900

    first dev commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'robosys2025_assignment_2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Shogo Yamashita',
+    maintainer_email='s24c1130pf@s.chibakoudai.jp',
+    description='a package for assignment',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
