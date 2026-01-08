0c13dbfb7e601939a152bf40e2246f8a3964d36b
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2026-01-08 23:30:43 +0900

    カメラ入力を行うhand_inputをテストから除外

--- test/test.bash
+++ test/test.bash
@@ -26,18 +26,17 @@ colcon build --packages-select robosys2025_assignment_2
 source $dir/.bashrc
 
 
-### hand_inputの出力テスト ###
-timeout 10 ros2 run robosys2025_assignment_2 hand_input &
-sleep 3
-ros2 topic echo /finger_close_state --once > data.tmp
-cat data.tmp | tr -d "\n -" | grep "00000"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-wait
-
-
 ### janken_outputの入力テスト ###
 timeout 30 ros2 run robosys2025_assignment_2 janken_output &
 sleep 3
+
+## トピックチェック ##
+ros2 topic list | grep "janken_result"
+[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
+
+## メッセージ型チェック ##
+ros2 topic info /janken_result | grep "std_msgs/msg/String"
+[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
 
 ## 正常入力 ##
 ros2 topic pub /finger_close_state std_msgs/msg/Int16MultiArray "{data: [1,1,1,1,1]}" -1
@@ -74,9 +73,17 @@ rm out.tmp
 wait
 
 
-### janken_outputの入力テスト ###
+### number_outputの入力テスト ###
 timeout 60 ros2 run robosys2025_assignment_2 number_output &
 sleep 3
+
+## トピックチェック ##
+ros2 topic list | grep "number_result"
+[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
+
+## メッセージ型チェック ##
+ros2 topic info /number_result | grep "std_msgs/msg/Int16"
+[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
 
 ## 正常入力 ##
 ros2 topic pub /finger_close_state std_msgs/msg/Int16MultiArray "{data: [1,1,1,1,1]}" -1
@@ -129,59 +136,6 @@ rm out.tmp
 wait
 
 
-### janken_out.launchのテスト ###
-timeout 20 ros2 launch robosys2025_assignment_2 janken_out.launch.py &
-sleep 3
-
-## トピックチェック ##
-ros2 topic list | grep "finger_close_state"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-ros2 topic list | grep "janken_result"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-
-## メッセージ型チェック ##
-ros2 topic info /finger_close_state | grep "std_msgs/msg/Int16MultiArray"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-ros2 topic info /janken_result | grep "std_msgs/msg/String"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-
-## データチェック ##
-ros2 topic echo /finger_close_state --once>data.tmp
-cat data.tmp | tr -d "\n -" | grep "00000"
-ros2 topic echo /janken_result --once>data.tmp
-cat data.tmp | grep -E "Rock|Scissors|Paper|None"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-rm data.tmp
-wait
-
-
-### number_out.launchのテスト ###
-timeout 20 ros2 launch robosys2025_assignment_2 number_out.launch.py &
-sleep 3
-
-## トピックチェック ##
-ros2 topic list | grep "finger_close_state"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-ros2 topic list | grep "number_result"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-
-## メッセージ型チェック ##
-ros2 topic info /finger_close_state | grep "std_msgs/msg/Int16MultiArray"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-ros2 topic info /number_result | grep "std_msgs/msg/Int16"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-
-## データチェック ##
-ros2 topic echo /finger_close_state --once>data.tmp
-cat data.tmp | tr -d "\n -" | grep "00000"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-ros2 topic echo /number_result --once>data.tmp
-cat data.tmp | grep -E -- "-1|0|1|2|3|4|5"
-[ "$?" = 0 ] || { ng_line="$LINENO" ; ng "$ng_line"; }
-rm data.tmp
-wait
-
-
 ### テスト結果 ###
 ng "$ng_line"
 if [ "${res}" = 0 ]; then
@@ -190,4 +144,5 @@ else
     echo -e "\e[31mTest failed\e[0m"
 fi
 
+wait
 exit $res
